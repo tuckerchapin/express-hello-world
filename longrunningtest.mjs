135ba14baa9106ff81025c6aa50fd48cee216198
Tucker Chapin <tuckerchapin@gmail.com>
2025-05-06 16:11:58 -0400

    Back to not sleeping, should double up

--- longrunningtest.mjs
+++ longrunningtest.mjs
@@ -1,10 +1,9 @@
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-const ms = 1000 * 5;
-// Math.floor(Math.random() * 1000 * 30);
+const ms = Math.floor(Math.random() * 1000 * 30);
 
 console.log(`Sleeping for ${ms / 1000}s...`);
-await sleep(ms);
-// console.log("NOT SLEEPING");
+// await sleep(ms);
+console.log("NOT SLEEPING");
 console.log("Done dummy test");
 // throw new Error("Dummy test error");
