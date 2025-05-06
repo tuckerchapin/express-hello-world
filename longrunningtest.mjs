51e131cbe5c9c82100c37970ab10c2a2d0efb556
Tucker Chapin <tuckerchapin@gmail.com>
2025-05-06 16:04:07 -0400

    Long running check now delayed by 2m

--- longrunningtest.mjs
+++ longrunningtest.mjs
@@ -1,9 +1,10 @@
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-const ms = Math.floor(Math.random() * 1000 * 30);
+const ms = 1000 * 60 * 2;
+// Math.floor(Math.random() * 1000 * 30);
 
 console.log(`Sleeping for ${ms / 1000}s...`);
-// await sleep(ms);
-console.log("NOT SLEEPING");
+await sleep(ms);
+// console.log("NOT SLEEPING");
 console.log("Done dummy test");
 // throw new Error("Dummy test error");
