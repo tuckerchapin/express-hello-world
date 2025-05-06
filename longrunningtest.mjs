28857cd616e1d663110d32f9e05572225369f1dd
Tucker Chapin <tuckerchapin@gmail.com>
2025-05-06 16:10:31 -0400

    Now 5s apart...

--- longrunningtest.mjs
+++ longrunningtest.mjs
@@ -1,6 +1,6 @@
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-const ms = 1000 * 60 * 5;
+const ms = 1000 * 5;
 // Math.floor(Math.random() * 1000 * 30);
 
 console.log(`Sleeping for ${ms / 1000}s...`);
