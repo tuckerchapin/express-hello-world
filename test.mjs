784d8e382b38f9f39a9e7d570451a2dee68b740b
Tucker Chapin <tuckerchapin@gmail.com>
2026-04-17 12:28:28 -0400

    now checks are sleepy

--- test.mjs
+++ test.mjs
@@ -4,8 +4,8 @@ const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const ms = Math.floor(Math.random() * 1000 * 30);
 
-// console.log(`Sleeping for ${ms / 1000}s...`);
-// await sleep(ms);
+console.log(`Sleeping for ${ms / 1000}s...`);
+await sleep(ms);
 
 // throw new Error("This commit is breaking");
 console.log("Dummy test passed!");
