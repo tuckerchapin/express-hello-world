204c5f5467d4ca627f44448a4bebb8ad444f8f96
Tucker Chapin <tuckerchapin@gmail.com>
2026-04-17 12:50:04 -0400

    communicate intention of test outcome before sleep

--- test.mjs
+++ test.mjs
@@ -2,12 +2,20 @@ console.log("Dummy test");
 
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
+shouldFail = false;
+if (Math.random() > 0.5) {
+  shouldFail = true;
+  console.log("Test will fail");
+} else {
+  console.log("Test will pass");
+}
+
 const ms = Math.floor(Math.random() * 1000 * 30);
 
 console.log(`Sleeping for ${ms / 1000}s...`);
 await sleep(ms);
 
-if (Math.random() > 0.5) {
+if (shouldFail) {
   throw new Error("Dummy test failed!");
 }
 console.log("Dummy test passed!");
